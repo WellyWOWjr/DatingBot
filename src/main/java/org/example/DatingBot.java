35b03b772ee1641e1bc607e18602695934a5df64
wellyWow <62172622+wellyWow@users.noreply.github.com>
2023-06-22 16:24:17 +0300

    refactoring + inline yes no with profile cycle (#1)
    * refactoring + inline yes no with profile cycle
    
    * убрал чепуху из урла вебхука
    
    * Add comments
    
    ---------
    
    Co-authored-by: Sasha <kravchik682@gmail.com>

--- src/main/java/org/example/DatingBot.java
+++ src/main/java/org/example/DatingBot.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -12,10 +14,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.starter.SpringWebhookBot;
 
 
-//https://api.telegram.org/bot6216527032:AAHLBgtiiyHK6ZMkIluoiFi6PjpZsictQUk/setWebhook?url=https://datingbot.onrender.com
+//https://api.telegram.org/bot6216527032:AAHLBgtiiyHK6ZMkIluoiFi6PjpZsictQUk/setWebhook?url=https://edd9-46-53-253-234.ngrok-free.app
 @Component
 public class DatingBot extends SpringWebhookBot {
-    public static String PATH = "https://datingbot.onrender.com";
+    private static final Logger log = LoggerFactory.getLogger(DatingBot.class);
+
+    public static String PATH = "https://edd9-46-53-253-234.ngrok-free.app";
     Map<Long, DatingRunner> chats = new HashMap<>();
 
     public DatingBot() {
@@ -26,40 +30,72 @@ public class DatingBot extends SpringWebhookBot {
 
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        System.out.println(update);
-        if (update.getMessage().isCommand()) {
-            return handleCommand(update.getMessage());
+        log.info("Initial update {}", update);
+        // = sout (вывод информации в консоль)
+        try {
+            Message message = getMessage(update);
+            if (chats.get(message.getChatId()) == null) {
+                return runStart(message);
+                //нет профиля - создаем
+            }
+            if (update.hasCallbackQuery()) {
+                log.info("Callback");
+                DatingRunner runner = chats
+                        .get(update.getCallbackQuery().getMessage().getChatId());
+                BotApiMethod<?> callbackResponse = runner.runCallBack(update);
+                execute(callbackResponse);
+                return runner.runShow(message);
+                //если есть Callback, создаем runner для конкретного ChatId и метод callbackResponse
+            }
+            if (update.getMessage().isCommand()) {
+                return handleCommand(update.getMessage());
+                //если ввели команду, выполняем соответствующий метод
+            }
+            return handleMessage(update.getMessage());
+            //ни один if не подходит - выполняем handleMessage()
+        } catch (Exception e) {
+            log.error("GG", e);
+            //если сообщение не получено(handleMessage() возвращает null) выводим GG в консоль
         }
-        return handleMessage(update.getMessage());
+        return new SendMessage();
+    }
+
+    private Message getMessage(Update update) {
+        Message message;
+        if (update.getMessage() != null) {
+            message = update.getMessage();
+        } else {
+            message = update.getCallbackQuery().getMessage();
+        }
+        return message;
+        //если message - сообщение, то возвращаем его, иначе нажали на клавиатуру, message = callback, return message
     }
 
     private BotApiMethod<?> handleCommand(Message message) {
         String command = message.getText();
         switch (command) {
             case "/start": {
-                chats.put(message.getChatId(), new DatingRunner());
-                String response = chats.get(message.getChatId()).runStart();
-                return new SendMessage(message.getChatId().toString(), response);
+                return runStart(message);
             }
             case "/me": {
-                String response;
-                if (chats.get(message.getChatId()) != null) {
-                    response = chats.get(message.getChatId()).runMe();
-                } else {
-                    response = "You don't have profile.\uD83E\uDEE0";
-                }
-                return new SendMessage(message.getChatId().toString(), response);
+                return chats.get(message.getChatId()).runMe(message);
+            }
+            case "/show": {
+                return chats.get(message.getChatId()).runShow(message);
             }
         }
         //Todo
         return null;
     }
 
+    private BotApiMethod<?> runStart(Message message) {
+        chats.put(message.getChatId(), new DatingRunner());
+        return chats.get(message.getChatId()).runStart(message);
+    }
+
 
     private BotApiMethod<?> handleMessage(Message message) {
-        String request = message.getText();
-        String response = chats.get(message.getChatId()).run(request);
-        return new SendMessage(message.getChatId().toString(), response);
+        return chats.get(message.getChatId()).run(message);
     }
 
 
